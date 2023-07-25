65a4b966c4ac1d1e73fe2675fa388067d9b83094
mahendra sahu <117085794+sahu-mahendra@users.noreply.github.com>
2023-07-26 00:36:03 +0530

    Update SwaggerConfig.java

--- src/main/java/com/educative/ecommerce/Config/SwaggerConfig.java
+++ src/main/java/com/educative/ecommerce/Config/SwaggerConfig.java
@@ -1,5 +1,5 @@
 ///*
-//package com.educative.ecommerce.Config;
+package com.educative.ecommerce.Config;
 //
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
@@ -11,9 +11,9 @@
 //import springfox.documentation.spi.DocumentationType;
 //import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-////@Configuration
-////@EnableSwagger2
+
+@Configuration
+@EnableSwagger2
 //public class SwaggerConfig {
 //
 //	@Bean
